175b9196b016c4ce183cf836717d3aaa3c2a3fbf
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-10-28 12:08:11 +0100

    Changing background color finished

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 class Counter extends React.Component {
 
-    state = {seconds: 40, color: 'blue'};
+    state = {seconds: 40};
+    color = 'blue';
 
     componentDidMount() {
         setInterval(() => {
             const seconds = this.state;
             if (this.state.seconds > 0) {
-                this.setState(({ seconds }) => ({
+                this.setState(({seconds}) => ({
                     seconds: seconds - 1
                 }))
             }
@@ -16,14 +17,27 @@ class Counter extends React.Component {
         }, 1000)
     }
 
-
-    render() {
-        const {seconds, color} = this.state;
+    renderCounter() {
+        const {seconds} = this.state;
+        if (seconds > 20) {
+            this.color = 'blue'
+        } else if (seconds > 12) {
+            this.color = 'orange';
+        } else {
+            this.color = 'red'
+        }
         return (
-            <div className="col-md-6 mx-auto">
-                { seconds < 10 ? `0${ seconds }` : seconds }
+            <div className="col-md-6 mx-auto py-4 text-white" style={{backgroundColor: `${this.color}`}}>
+                {seconds < 10 ? `0${seconds}` : seconds}
             </div>
         );
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.renderCounter()}
+            </React.Fragment>);
     }
 }
 
