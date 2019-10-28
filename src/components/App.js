7342226847ee55f3cb18a3a59863c6e380ecf566
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-10-28 11:27:31 +0100

    Initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Counter from './Counter';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                Hello
+            </div>
+        );
+    }
+}
+
+export default App;
